e9dae3523350d6794f5188578fd0a33171d6ee62
Abdulaziz Otyfah <otyfah@gmail.com>
2023-02-28 09:18:56 +0300

    project mangment

--- projects/urls.py
+++ projects/urls.py
@@ -5,6 +5,13 @@ from . import views
 
 urlpatterns = [
     path('', views.ProjectListView.as_view(), name='project_list'),
-    path('project/create', views.ProjectCreateView.as_view(), name='project_create')
+    path('project/create', views.ProjectCreateView.as_view(), name='project_create'),
+    path('project/edit/<int:pk>', views.ProjectUpdateView.as_view(), name='project_update'),
+    path('project/delete/<int:pk>', views.ProjectDeleteView.as_view(), name='project_delete'),
+
+    #لن نستخدم هذا الرابط فقط كبوست POST نضيف خاصية البوست في ملف الفيو
+    path('task/create', views.TaskCreateView.as_view(), name='task_create'),
+    path('task/edit/<int:pk>', views.TaskUpdateView.as_view(), name='task_update'),
+    path('task/delete/<int:pk>', views.TaskDeleteView.as_view(), name='task_delete'),
 
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView ,CreateView
-from django.urls import reverse_lazy
+from django.views.generic import ListView ,CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy ,reverse
 from . import models
 from . import forms
 
@@ -18,3 +18,47 @@ class ProjectCreateView(CreateView):
     form_class = forms.ProjectCreateForm
     template_name = 'project/create.html'
     success_url = reverse_lazy('project_list')
+
+
+class ProjectUpdateView(UpdateView):
+    model = models.Projects
+    form_class = forms.ProjectUpdateForm
+    template_name = 'project/update.html'
+#يعيد صفحة المشروع نفسها
+#object  يععبر عن المشروع الحالي هو  prpject
+#يعدل في نفس الصفحة
+
+    def get_success_url(self):
+        return reverse('project_update', args=[self.object.project.id])
+
+
+class ProjectDeleteView(DeleteView):
+    model = models.Projects
+    template_name = 'project/delete.html'
+    success_url = reverse_lazy('project_list')
+
+
+#لن نحتاج الي فورم خاص سوف نضيفها في نفس فورم المشاريع
+class TaskCreateView(CreateView):
+    model = models.Task
+    fields = ['project', 'description']
+    http_method_names = ['post']
+
+    def get_success_url(self):
+        return reverse('project_update', args=[self.object.project.id])
+
+
+class TaskUpdateView(UpdateView):
+    model = models.Task
+    fields = ['is_completed']
+    http_method_names = ['post']
+
+    def get_success_url(self):
+        return reverse('project_update', args=[self.object.project.id])
+
+
+class TaskDeleteView(DeleteView):
+    model = models.Task
+
+    def get_success_url(self):
+        return reverse('project_update', args=[self.object.project.id])
